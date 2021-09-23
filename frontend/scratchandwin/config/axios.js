9e3e4ca16e3867826699a276fb164ef0823e8f52
katdom13 <katdom13@gmail.com>
2021-09-23 16:58:59 +0800

    Finished basic frontend

---
+++ frontend/scratchandwin/config/axios.js
@@ -0,0 +1,35 @@
+import axios from "axios"
+
+import { apiUrl } from "./apiUrl"
+
+const instance = axios.create({
+  baseURL: apiUrl,
+  timeout: 12000,
+  headers: {
+    "Content-Type": "application/json",
+    accept: "application/json",
+  },
+})
+
+const invoke = async (url, method = "get", data = {}, csrf = "") => {
+  return instance({
+    method: method,
+    url: url,
+    data: data,
+    headers: {
+      ...instance.defaults.headers,
+      "X-CSRFToken": csrf,
+    },
+  })
+    .then((response) => Promise.resolve(response.data))
+    .catch((error) => Promise.reject(error))
+}
+
+// Promises
+
+const submitEntry = async (code, mobile) => {
+  const data = { code, mobile }
+  return invoke("promo/", "post", data)
+}
+
+export { submitEntry }
--- frontend/scratchandwin/pages/_app.js
+++ frontend/scratchandwin/pages/_app.js
@@ -1,7 +1,29 @@
+import React from "react"
 import "../styles/globals.css"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { CacheProvider } from "@emotion/react"
+import CssBaseline from "@mui/material/CssBaseline"
+import Head from "next/head"
+
+import createEmotionCache from "../config/createEmotionCache"
+
+// Client-side cache, shared for the whole session of the user in the the browser
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Scratch and win</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <CssBaseline />
+      <Component {...pageProps} />
+    </CacheProvider>
+  )
 }
 
 export default MyApp

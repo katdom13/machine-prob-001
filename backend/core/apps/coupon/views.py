f32c80f65c520d7153face5f3bce5b0db85f436b
katdom13 <katdom13@gmail.com>
2021-09-24 15:24:08 +0800

    Edited file read

--- backend/core/apps/coupon/views.py
+++ backend/core/apps/coupon/views.py
@@ -1,7 +1,12 @@
+import os
+
 from core.apps.coupon.models import Coupon
+from django.conf import settings
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .storage import OverwriteStorage
 
 
 class UploadCouponList(APIView):
@@ -11,10 +16,14 @@ class UploadCouponList(APIView):
         rewrite = request.GET.get('rewrite')
         is_rewrite = False
         file = request.FILES.get('file')
-        codes = [line.decode('utf-8').strip() for line in file.readlines()]
-        response = {
-            'success': 'All coupons uploaded to the database.'
-        }
+
+        fs = OverwriteStorage()
+        fs.save(file.name, file)
+
+        saved_file = os.path.join(settings.MEDIA_ROOT, file.name)
+
+        with open(saved_file, 'r') as sfile:
+            codes = sfile.read().splitlines()
 
         if rewrite:
             is_rewrite = True if rewrite.lower() in ['true', '1'] else False
@@ -28,4 +37,6 @@ class UploadCouponList(APIView):
             if not coupon:
                 Coupon.objects.create(code=code)
 
-        return Response(response, status=200)
+        return Response({
+            'success': 'All coupons uploaded to the database.'
+        }, status=200)
